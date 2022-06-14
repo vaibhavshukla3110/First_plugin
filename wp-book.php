fb6cdb15410e30d9b49ae2394b540c32838605e3
Vaibhav Shukla <vaibhav.shukla@hbwsl.com>
2022-06-14 13:03:56 +0530

    make changes where error exists

--- wp-book.php
+++ wp-book.php
@@ -83,61 +83,61 @@ run_wp_book();
 
 
 
-add_action('init', 'wporg_book_init');
+add_action( 'init', 'wporg_book_init' );
 
 /**
  * Register a book post type.
  */
 
 function wporg_book_init() {
-    $labels = array(
-        'name'                  => _x( 'Books', 'Post type general name', 'textdomain' ),
-        'singular_name'         => _x( 'Book', 'Post type singular name', 'textdomain' ),
-        'menu_name'             => _x( 'Books', 'Admin Menu text', 'textdomain' ),
-        'name_admin_bar'        => _x( 'Book', 'Add New on Toolbar', 'textdomain' ),
-        'add_new'               => __( 'Add New', 'textdomain' ),
-        'add_new_item'          => __( 'Add New Book', 'textdomain' ),
-        'new_item'              => __( 'New Book', 'textdomain' ),
-        'edit_item'             => __( 'Edit Book', 'textdomain' ),
-        'view_item'             => __( 'View Book', 'textdomain' ),
-        'all_items'             => __( 'All Books', 'textdomain' ),
-        'search_items'          => __( 'Search Books', 'textdomain' ),
-        'parent_item_colon'     => __( 'Parent Books:', 'textdomain' ),
-        'not_found'             => __( 'No books found.', 'textdomain' ),
-        'not_found_in_trash'    => __( 'No books found in Trash.', 'textdomain' ),
-        'featured_image'        => _x( 'Book Cover Image', 'Overrides the “Featured Image” phrase for this post type. Added in 4.3', 'textdomain' ),
-        'set_featured_image'    => _x( 'Set cover image', 'Overrides the “Set featured image” phrase for this post type. Added in 4.3', 'textdomain' ),
-        'remove_featured_image' => _x( 'Remove cover image', 'Overrides the “Remove featured image” phrase for this post type. Added in 4.3', 'textdomain' ),
-        'use_featured_image'    => _x( 'Use as cover image', 'Overrides the “Use as featured image” phrase for this post type. Added in 4.3', 'textdomain' ),
-        'archives'              => _x( 'Book archives', 'The post type archive label used in nav menus. Default “Post Archives”. Added in 4.4', 'textdomain' ),
-        'insert_into_item'      => _x( 'Insert into book', 'Overrides the “Insert into post”/”Insert into page” phrase (used when inserting media into a post). Added in 4.4', 'textdomain' ),
-        'uploaded_to_this_item' => _x( 'Uploaded to this book', 'Overrides the “Uploaded to this post”/”Uploaded to this page” phrase (used when viewing media attached to a post). Added in 4.4', 'textdomain' ),
-        'filter_items_list'     => _x( 'Filter books list', 'Screen reader text for the filter links heading on the post type listing screen. Default “Filter posts list”/”Filter pages list”. Added in 4.4', 'textdomain' ),
-        'items_list_navigation' => _x( 'Books list navigation', 'Screen reader text for the pagination heading on the post type listing screen. Default “Posts list navigation”/”Pages list navigation”. Added in 4.4', 'textdomain' ),
-        'items_list'            => _x( 'Books list', 'Screen reader text for the items list heading on the post type listing screen. Default “Posts list”/”Pages list”. Added in 4.4', 'textdomain' ),
-    );
- 
-    $args = array(
-        'labels'             => $labels,
-        'public'             => true,
-        'publicly_queryable' => true,
-        'show_ui'            => true,
-        'show_in_menu'       => true,
-        'query_var'          => true,
-        'rewrite'            => array( 'slug' => 'book' ),
-        'capability_type'    => 'post',
-        'has_archive'        => true,
-        'hierarchical'       => false,
-        'menu_position'      => null,
-        'supports'           => array( 'title', 'editor', 'author', 'thumbnail', 'excerpt', 'comments' ),
-    );
- 
-    register_post_type( 'book', $args );
+	$labels = array(
+		'name'                  => _x( 'Books', 'Post type general name', 'textdomain' ),
+		'singular_name'         => _x( 'Book', 'Post type singular name', 'textdomain' ),
+		'menu_name'             => _x( 'Books', 'Admin Menu text', 'textdomain' ),
+		'name_admin_bar'        => _x( 'Book', 'Add New on Toolbar', 'textdomain' ),
+		'add_new'               => __( 'Add New', 'textdomain' ),
+		'add_new_item'          => __( 'Add New Book', 'textdomain' ),
+		'new_item'              => __( 'New Book', 'textdomain' ),
+		'edit_item'             => __( 'Edit Book', 'textdomain' ),
+		'view_item'             => __( 'View Book', 'textdomain' ),
+		'all_items'             => __( 'All Books', 'textdomain' ),
+		'search_items'          => __( 'Search Books', 'textdomain' ),
+		'parent_item_colon'     => __( 'Parent Books:', 'textdomain' ),
+		'not_found'             => __( 'No books found.', 'textdomain' ),
+		'not_found_in_trash'    => __( 'No books found in Trash.', 'textdomain' ),
+		'featured_image'        => _x( 'Book Cover Image', 'Overrides the “Featured Image” phrase for this post type. Added in 4.3', 'textdomain' ),
+		'set_featured_image'    => _x( 'Set cover image', 'Overrides the “Set featured image” phrase for this post type. Added in 4.3', 'textdomain' ),
+		'remove_featured_image' => _x( 'Remove cover image', 'Overrides the “Remove featured image” phrase for this post type. Added in 4.3', 'textdomain' ),
+		'use_featured_image'    => _x( 'Use as cover image', 'Overrides the “Use as featured image” phrase for this post type. Added in 4.3', 'textdomain' ),
+		'archives'              => _x( 'Book archives', 'The post type archive label used in nav menus. Default “Post Archives”. Added in 4.4', 'textdomain' ),
+		'insert_into_item'      => _x( 'Insert into book', 'Overrides the “Insert into post”/”Insert into page” phrase (used when inserting media into a post). Added in 4.4', 'textdomain' ),
+		'uploaded_to_this_item' => _x( 'Uploaded to this book', 'Overrides the “Uploaded to this post”/”Uploaded to this page” phrase (used when viewing media attached to a post). Added in 4.4', 'textdomain' ),
+		'filter_items_list'     => _x( 'Filter books list', 'Screen reader text for the filter links heading on the post type listing screen. Default “Filter posts list”/”Filter pages list”. Added in 4.4', 'textdomain' ),
+		'items_list_navigation' => _x( 'Books list navigation', 'Screen reader text for the pagination heading on the post type listing screen. Default “Posts list navigation”/”Pages list navigation”. Added in 4.4', 'textdomain' ),
+		'items_list'            => _x( 'Books list', 'Screen reader text for the items list heading on the post type listing screen. Default “Posts list”/”Pages list”. Added in 4.4', 'textdomain' ),
+	);
+
+	$args = array(
+		'labels'             => $labels,
+		'public'             => true,
+		'publicly_queryable' => true,
+		'show_ui'            => true,
+		'show_in_menu'       => true,
+		'query_var'          => true,
+		'rewrite'            => array( 'slug' => 'book' ),
+		'capability_type'    => 'post',
+		'has_archive'        => true,
+		'hierarchical'       => false,
+		'menu_position'      => null,
+		'supports'           => array( 'title', 'editor', 'author', 'thumbnail', 'excerpt', 'comments' ),
+	);
+
+	register_post_type( 'book', $args );
 }
 
 
 
-add_action( 'init', 'wporg_register_taxonomy_book_category');
+add_action( 'init', 'wporg_register_taxonomy_book_category' );
 
 
 /**
@@ -146,17 +146,17 @@ add_action( 'init', 'wporg_register_taxonomy_book_category');
 
 function wporg_register_taxonomy_book_category() {
 	$labels = array(
-		 'name'              => _x( 'Book Category', 'taxonomy general name' ),
-         'singular_name'     => _x( 'Book Category', 'taxonomy singular name' ),
-         'search_items'      => __( 'Search Book Category' ),
-         'all_items'         => __( 'All Book Categories' ),
-         'parent_item'       => __( 'Parent Category' ),
-         'parent_item_colon' => __( 'Parent Category:' ),
-         'edit_item'         => __( 'Edit Category' ),
-         'update_item'       => __( 'Update Category' ),
-         'add_new_item'      => __( 'Add New Category' ),
-         'new_item_name'     => __( 'New Category Name' ),
-         'menu_name'         => __( 'Book Category' ),
+		'name'              => _x( 'Book Category', 'taxonomy general name' ),
+		'singular_name'     => _x( 'Book Category', 'taxonomy singular name' ),
+		'search_items'      => __( 'Search Book Category' ),
+		'all_items'         => __( 'All Book Categories' ),
+		'parent_item'       => __( 'Parent Category' ),
+		'parent_item_colon' => __( 'Parent Category:' ),
+		'edit_item'         => __( 'Edit Category' ),
+		'update_item'       => __( 'Update Category' ),
+		'add_new_item'      => __( 'Add New Category' ),
+		'new_item_name'     => __( 'New Category Name' ),
+		'menu_name'         => __( 'Book Category' ),
 	);
 	$args   = array(
 		'hierarchical'      => true, // make it hierarchical (like categories)
@@ -164,14 +164,14 @@ function wporg_register_taxonomy_book_category() {
 		'show_ui'           => true,
 		'show_admin_column' => true,
 		'query_var'         => true,
-		'rewrite'           => [ 'slug' => 'Book Category' ],
+		'rewrite'           => array( 'slug' => 'Book Category' ),
 	);
-	register_taxonomy( 'book category' , array('book'), $args );
+	register_taxonomy( 'book category', array( 'book' ), $args );
 }
 
 
 
-add_action( 'init', 'wporg_register_taxonomy_book_tag');
+add_action( 'init', 'wporg_register_taxonomy_book_tag' );
 
 
 /**
@@ -180,29 +180,29 @@ add_action( 'init', 'wporg_register_taxonomy_book_tag');
 
 function wporg_register_taxonomy_book_tag() {
 	$labels = array(
-		'name'              => _x( 'Book Tag', 'taxonomy general name' ),
-		'singular_name'     => _x( 'Book Tag', 'taxonomy singular name' ),
-		'search_items'      => __( 'Search Book Tag' ),
-		'all_items'         => __( 'All Book Tags' ),
-		'parent_item'       => null,
-    	'parent_item_colon' => null,
-		'edit_item'         => __( 'Edit Tag' ),
-		'update_item'       => __( 'Update Tag' ),
-		'add_new_item'      => __( 'Add New Tag' ),
-		'new_item_name'     => __( 'New Tag Name' ),
+		'name'                       => _x( 'Book Tag', 'taxonomy general name' ),
+		'singular_name'              => _x( 'Book Tag', 'taxonomy singular name' ),
+		'search_items'               => __( 'Search Book Tag' ),
+		'all_items'                  => __( 'All Book Tags' ),
+		'parent_item'                => null,
+		'parent_item_colon'          => null,
+		'edit_item'                  => __( 'Edit Tag' ),
+		'update_item'                => __( 'Update Tag' ),
+		'add_new_item'               => __( 'Add New Tag' ),
+		'new_item_name'              => __( 'New Tag Name' ),
 		'separate_items_with_commas' => __( 'Separate topics with commas' ),
-		'add_or_remove_items' => __( 'Add or remove topics' ),
-		'choose_from_most_used' => __( 'Choose from the most used topics' ),
-		'menu_name'         => __( 'Book Tag' ),
+		'add_or_remove_items'        => __( 'Add or remove topics' ),
+		'choose_from_most_used'      => __( 'Choose from the most used topics' ),
+		'menu_name'                  => __( 'Book Tag' ),
 	);
 	$args   = array(
 		'hierarchical'      => false,
 		'labels'            => $labels,
 		'show_ui'           => true,
-		'show_in_rest'	    => true,
+		'show_in_rest'      => true,
 		'show_admin_column' => true,
 		'query_var'         => true,
-		'rewrite'           => [ 'slug' => 'Book Tag' ],
+		'rewrite'           => array( 'slug' => 'Book Tag' ),
 	);
-	register_taxonomy( 'book tag' , array('book'), $args );
+	register_taxonomy( 'book tag', array( 'book' ), $args );
 }
